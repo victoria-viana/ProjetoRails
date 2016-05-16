a8a74686b1167e30b54a45fe3245af679bd48f3e
victoria-viana <victoriarviana@gmail.com>
2016-05-16 17:39:17 -0300

    Alteracoes no model

--- test/models/coach_test.rb
+++ test/models/coach_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class CoachTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ def setup
+    @coach = Coach.new(name: "Example User", email: "user@example.com", cpf: "000.000.000-00")
+  end
+
+  test "should be valid" do
+    assert @coach.valid?
+  end
+test "name should not be too long" do
+    @coach.name = "a" * 51
+    assert_not @coach.valid?
+  end
+
+  test "email should not be too long" do
+    @coach.email = "a" * 244 + "@example.com"
+    assert_not @coach.valid?
+  end
+  test "email addresses should be unique" do
+    duplicate_user = @coach.dup
+    @coach.save
+    assert_not duplicate_coach.valid?
+  end
 end
+
+
